b987cf0abf3d11e116ca3828f30a1fd82abb5b14
Nolan Brady <nolanrbrady@gmail.com>
2021-10-27 15:42:48 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  const clickHandler = (item) => {
+    console.log(item);
+  }
+
+  const renderLODButtons = () => {
+    const lodOptions = [
+        {name: "LOD 200", id: "lod200"},
+        {name: 'LOD 300', id: "lod300"}
+      ];
+    return lodOptions.map((item) => {
+      return(
+        <div>       
+          <button onClick={() => clickHandler(item)} className="Button">
+          <p>{item.name}</p>
+        </button>
+        </div>
+      )
+    });
+  };
+
+  const renderPanelButtons = () => {
+    const panelOptions = [
+      {name: 'Panel 1', id: 'panel1'}, 
+      { name: 'Panel 2', id: 'panel2'},
+      { name: 'Panel 3', id: 'panel3'},
+    ];
+
+    return panelOptions.map(item => {
+      return (
+        <button onClick={() => clickHandler(item)} className="Button">
+          <p>{item.name}</p>
+        </button>
+      )
+    })
+    
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Select Level of Detail</h1>
+        <div className="Button-container">
+          {renderLODButtons()}
+        </div>
+        <h1>Select Panel</h1>
+        <div className="Button-container">
+          {renderPanelButtons()}
+        </div>       
       </header>
     </div>
   );
